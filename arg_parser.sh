e41e51a862043f83a537c6bc4ddae0158f049332
Jeffery Vahrenkamp <u6004424@hci.utah.edu>
2019-02-22 09:33:38 -0700

    arg_parser.sh added default values
    Added the ability to add default values optional arguments to arg_parser.sh

--- arg_parser.sh
+++ arg_parser.sh
@@ -1,60 +1,90 @@
-help_message () {
-echo -e "\n#######################################\n# Arg_parser.sh by Jeffery Vahrenkamp #\n#######################################\n\nUsage:\n\
-\n\
-\tInclude \"source ~u6004424/bin/arg_parser.sh\" at the top of the script\n\
-\tthe \"get_flags\" function is used to define flags to be parsed by script\n\
+#!/bin/bash
 
-\tDefining Flags:\n\
-\t\t-Anything before a flag will be saved under the variable "\$unflagged"\n\
-\t\t-Mandatory flags use the format variable=flag  example: Mandatory_argument=-m\n\
-\t\t-Optional flags use the format variable~flag  example: Optional_argument~-o\n\
-\n\tHelp Messages:
-\t\t-If a usage message is desired to be printed when mandatory flags are\n\
-\t\t    not present create a function called usage which echos your message \n\
-\t\t-Usage function must be defined before get_flags function call!\n\
-\t\t\033[4m\033[32;1m-NOTE:The -h flag is reserved for calling the usage function without error messages!\033[0m\n\
-\n\
-Example.sh script:\n\
-       source ~u6004424/bin/arg_parser.sh\n\
-\n\
-       usage () {\n\
-               echo -e \"this is a usage or help message\"\n\
-       }\n\
-\n\
-       get_flags optional~-o mandatory=-m \n\
-\n\
-       echo optional=\$optional\n\
-       echo mandatory=\$mandatory\n\
-       echo unflagged=\$unflagged\n\
-\n\
----------------------------------------------\n\
-\$:bash Example.sh no_flag -o optional1 option2 -m this_is_mandatory\n\
-\nOutput:\n\
-\toptional=optional1 option2\n\
-\tmandatory=this_is_mandatory\n\
-\tunflagged=no_flag\n\
+help_message () {
+cat <<message
+
+#######################################
+# Arg_parser.sh by Jeffery Vahrenkamp #
+#######################################
+
+Usage:
+
+Include "source FILE_LOCATION/arg_parser.sh" at the top of the script
+the "get_flags" function is used to define flags to be parsed by script
+
+Defining Flags:
+	-Anything before a flag will be saved under the variable "\$unflagged"
+	-Mandatory flags use the format variable=flag	
+		Example: Mandatory_argument=-m
+	-Optional flags use the format variable~flag:default  
+		Example: Optional_argument~-o:default_value
+	-;; in default values will be converted to space.
+       		Example: Optional~-o:val1;;val2;;val3 will become \$Optional=val1 val2 val3	
+
+Help Messages:
+	-If a usage message is desired to be printed when mandatory flags are
+		not present create a function called usage which echos your message
+	-Usage function must be defined before get_flags function call!
+		\033[4m\033[32;1m-NOTE:The -h flag is reserved for calling the usage function without error messages!\033[0m
+
+Example.sh script:
+       source FILE_LOCATION/arg_parser.sh
+
+       usage () {
+               cat << help_message
+put your usage message here
+formatting will be preserved
+
+help_message
+       }
+
+       get_flags optional~-o optional_default~-d:MyOption optional_nodefault~-n mandatory=-m 
+
+       echo optional=\$optional
+       echo optional_default=\$optional_default
+       echo optional_nodefault=\$optional_nodefault
+       echo mandatory=\$mandatory
+       echo unflagged=\$unflagged
+
+---------------------------------------------
+\$:bash Example.sh no_flag -o optional1 option2 -m this_is_mandatory
+Output:
+	optional=optional1 option2
+	optional_default=MyOption
+	optional_nodefault=
+	mandatory=this_is_mandatory
+	unflagged=no_flag
+
 "
+message
 }
 
 __args=$@
 
 [[ $__args == "" ]] && __args="-h"
 
+
 get_flags () {
-	eval $(echo $@ $__args | awk 'BEGIN{
+	eval $(echo $@ $__args | awk -v inargs="$__args" ' 
+		BEGIN{
 			flag["--unflagged"]
 			working="--unflagged"
 		}
-		{for (x=1;x<=NF;x++) {
+		{
+			for (x=1;x<=NF;x++) {
 			if ($x ~ "~") {
-				split($x,out,"~")
+				split($x,out,"[:~ ]")
 				optional_keys[out[1]]=out[2]
+				if (length(out)>2){
+					gsub(";;"," ",out[3])
+					default_value[out[1]]=out[3]
+				}
 			}
 			else if ($x ~ "=") {
 				split($x,out,"=") 
 				mandatory_keys[out[1]]=out[2]
 			}
-			else if ($x~"^-") {
+			else if ($x ~ "^-.+") {
 				flag[$x]
 				working=$x
 			}
@@ -64,7 +94,7 @@ get_flags () {
 	END{
 		quit=0
 		if ("-h" in flag) {
-			print "[[ `type usage 2> /dev/null` =~ \"function\" ]] && usage || [[ -z $usage ]] || echo -e $usage; exit 1"
+			print "[[ $(type usage 2> /dev/null) =~ function ]] && usage || [[ -z $usage ]] || echo -e $usage; exit 1"
 			exit 0
 		}
 		for (x in mandatory_keys) {
@@ -80,15 +110,18 @@ get_flags () {
 			}
 		}
 		if (quit > 0) {
-			print "[[ `type usage 2> /dev/null` =~ \"function\" ]] && usage || [[ -z $usage ]] || echo -e $usage; exit 1"
+			print "[[ $(type usage 2> /dev/null) =~ function ]] && usage || [[ -z $usage ]] || echo -e $usage; exit 1"
 			exit 1
 		}
 		for (x in optional_keys) {
 			sub("^ ","",flag[optional_keys[x]])
-			print x"=\""flag[optional_keys[x]]"\""
+			if (length(flag[optional_keys[x]]) > 0 || inargs ~ optional_keys[x] ) 
+				print x"=\""flag[optional_keys[x]]"\""
+			else if ( !(inargs ~ optional_keys[x]) && (x in default_value)) 
+				print x"=\""default_value[x]"\""
 			}
 		sub("^ ","",flag["--unflagged"])
-		print "unflagged=\""flag["--unflagged"]"\""
+		if (length(flag[--unflagged])>0) print "unflagged=\""flag["--unflagged"]"\""
 	}')
 }
 
@@ -97,32 +130,3 @@ get_flags () {
 [[ "${BASH_SOURCE[0]}" != "${0}" ]] || help_message
 
 
-#usage:
-# map flags to variable like this:
-# Anything before a flag will be saved under the variable "$unflagged"
-# mandatory flags use = : Mandatory_argument=-m
-# optional flags use ~ : Optional_argument~-o 
-# last argument must be $@!
-# if a usage message is desired to be printed when mandator flags are not present create a function called usage which echos your message or create a variable called "usage" that contains your usage message
-# Usage function/variable must be before get_flags function call!
-#
-#Example script: 
-# 	source ~u6004424/bin/arg_parser.sh
-#	
-#	usage () {
-#		echo -e "this is a ussage/help message"
-#	{
-#
-# 	get_flags optional~-o mandatory=-m $@
-#
-# 	echo optional=$optional
-# 	echo mandatory=$mandatory
-# 	echo unflagged=$unflagged
-#
-#---------------------------------------------
-#  Example.sh no_flag -o this_is_option -m this_is_mandatory 
-#  Output:
-# 	optional=this_is_optional
-# 	mandatory=this_is_mandatory
-# 	unflagged=no_flag
-#
